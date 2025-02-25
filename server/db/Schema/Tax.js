ea2f393a1057b79ad33df24987901eb14665b43f
Ka Long Ngai <kls8162@gmail.com>
2025-02-24 21:00:25 -0500

    Patched: Wrong Tax_type ENUM

--- server/db/Schema/Tax.js
+++ server/db/Schema/Tax.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-import { TAX_STATUS, FILING_STATUS } from "./Enums";
+import { TAX_TYPE, FILING_STATUS } from "./Enums";
 
 const TaxBracketSchema = new mongoose.Schema({
     lowerBound: { type: Number, required: true },
@@ -9,7 +9,7 @@ const TaxBracketSchema = new mongoose.Schema({
 });
 
 const TaxSchema = new mongoose.Schema({
-    taxType: { type: String, enum: TAX_STATUS, required: true },
+    taxType: { type: String, enum: TAX_TYPE, required: true },
     filingStatus: { type: String, enum: FILING_STATUS, required: true },
     taxBrackets: [{ type: TaxBracketSchema, required: true }]
 });
