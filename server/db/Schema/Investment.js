f9f6905877da5d1023e7b157905c37c650439e2f
Ka Long Ngai <kls8162@gmail.com>
2025-02-27 23:52:26 -0500

    Modified: InvestmentType and Invest Object

--- server/db/Schema/Investment.js
+++ server/db/Schema/Investment.js
@@ -2,17 +2,30 @@ import mongoose from "mongoose";
 
 import { TAX_STATUS } from "./Enums";
 
-const InvestmentSchema = new mongoose.Schema({
+const InvestmentTypeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    value: { type: Number, required: true },
-    investmentType: { type: String, required: true },
     expectedAnnualReturn: { type: Number, required: true },
     expectedAnnualReturnDistribution: { type: mongoose.Schema.Types.ObjectId, ref: 'Distribution', required: true },
     expenseRatio: { type: Number, required: true },
     expectedAnnualIncome: { type: Number, required: true },
     expectedAnnualIncomeDistribution: { type: mongoose.Schema.Types.ObjectId, ref: 'Distribution', required: true },
     taxability: { type: Boolean, required: true },
+});
+
+InvestmentTypeSchema.virtual('id').get(function get() {
+    return this._id.toHexString();
+});
+InvestmentTypeSchema.set('toJSON', {
+    virtuals: true,
+});
+InvestmentTypeSchema.set('toObject', {
+    virtuals: true,
+});
+
+const InvestmentSchema = new mongoose.Schema({
+    value: { type: Number, required: true },
+    investmentType: { type: mongoose.Schema.Types.ObjectId, ref: 'InvestmentType', required: true },
     taxStatus: { type: String, enum: TAX_STATUS, required: true }
 });
 
@@ -27,4 +40,9 @@ InvestmentSchema.set('toObject', {
     virtuals: true,
 });
 
-export default mongoose.model('Investment', InvestmentSchema);
+const InvestmentType = mongoose.model('InvestmentType', InvestmentTypeSchema);
+
+
+const Investment = mongoose.model('Investment', InvestmentSchema);
+
+export default { InvestmentType, Investment };
