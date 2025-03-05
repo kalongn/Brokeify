e86b75cec3e0d4f1b6499309ccf951fb7c8b74dc
Ka Long Ngai <kls8162@gmail.com>
2025-03-05 15:57:20 -0500

    Created a sample connection make sure you have a .env file under server directory containing your mongoDB address.

---
+++ server/db/testSchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+// Connect to MongoDB
+const DB_ADDRESS = `${process.env.DB_ADDRESS}`;
+mongoose.connect(DB_ADDRESS);
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+connection.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+});
+
