d6f98a3be3c66fafa0bbdcc1216affdbba145a28
Ka Long Ngai <kls8162@gmail.com>
2025-03-08 12:03:14 -0500

    Modified: RMDTable Controller implement useful feature for the controller instead of boilderplate stuff.

--- server/db/controllers/RMDTableController.js
+++ server/db/controllers/RMDTableController.js
@@ -1,14 +1,22 @@
-import mongoose from "mongoose";
 import RMDTable from "../models/RMDTable.js";
-
-// TODO: This class is incomplete and will need to be completed later on once we figure out how many RMD table we actually need.
 
 /**
  * Controller for RMDTable, Support CRUD operations for RMDTable Class, DO NOT USE THIS CLASS YET
  */
 export default class RMDTableController {
+    /**
+     * Constructor (empty)
+     */
     constructor() { }
 
+    /**
+     * This function creates a new RMDTable with the given data
+     * @param {RMDTable} data 
+     * @returns 
+     *      Returns the created RMDTable
+     * @throws Error
+     *      Throws error if any error occurs
+     */
     async create(data) {
         try {
             const rmdTable = new RMDTable(data);
@@ -20,33 +28,42 @@ export default class RMDTableController {
         }
     }
 
-    async readAll() {
+    /**
+     * This function reads the RMDTable (we are assuming there's only one here since it is always up to date with the current one)
+     * @returns 
+     *      Returns the RMDTable
+     * @throws Error
+     *      Throws error if any error occurs
+     */
+    async read() {
         try {
-            return await RMDTable.find();
+            return await RMDTable.findOne();
         }
         catch (error) {
             throw new Error(error);
         }
     }
 
-    async read(id) {
+    /**
+     * This function updates the RMDTable with the given data (realistically this should run with a script that updates the RMDTable every year)
+     * @param {RMDTable} data 
+     * @returns 
+     *      Returns the updated RMDTable
+     * @throws Error
+     *     Throws error if RMDTable not found or if any error occurs
+     */
+    async update(data) {
         try {
-            return await RMDTable.findById(id);
+            const rmdTable = await RMDTable.findOne();
+            if (!rmdTable) {
+                throw new Error("RMDTable not found");
+            }
+            rmdTable.update(data);
+            await rmdTable.save();
+            return rmdTable;
         }
         catch (error) {
             throw new Error(error);
         }
-    }
-
-    async update(id, data) {
-        try {
-            return await RMDTable.findByIdAndUpdate(id, data, {
-                new: true
-            });
-        }
-        catch (error) {
-            throw new Error(error);
-        }
-
     }
 }
