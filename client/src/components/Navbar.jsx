4bf79559a76c7a3a4a8054ba23be8590861e6d94
Ka Long Ngai <63157613+kalongn@users.noreply.github.com>
2025-03-15 13:13:02 -0400

    [Frontend] Vite React Base Template
    1. The main.jsx should not be modified after this state (maybe just remove Strict when applicable)
    2. Index.css is for every global CSS setting
    3. Index.html should not be modified after this too
    4. App is where we set up Route and every page after that
    5. There's a reimplement NavBar component, which is follows by the module.css file, use MODULE CSS for any particular thing CSS
    6. pages folder is for an actual page, while components folder is for item reside in a page but use across different pages
    7. Imported out Icon

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// TODO: to be actually implement then random stuff here
+
+// import style from './Navbar.module.css';
+// import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <a href="www.google.come">Google</a>
+            <button>test</button>
+            <p>Welcome to the home page!</p>
+        </div>
+    );
+}
+
+export default Home;
