82643b20eb31a5987a55ff1cec7b8c1044abae97
Ka Long Ngai <63157613+kalongn@users.noreply.github.com>
2025-03-15 13:17:27 -0400

    [Backend] [Middleware] backend server setup (#7)
    1. Requires modification on .env file on both client and server folder
       - Client requires:
          - VITE_SERVER_ADDRESS
          Note: only variable starting with VITE is exposed to VITE, otherwise it is ignored.
       - Server requires:
          - DB_ADDRESS
          - SERVER_PORT
          - SECRET
          - CLIENT_URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import './App.css'
+import { useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
+import Axios from 'axios';
+
+import './App.css'
 
 import Home from './pages/Home.jsx';
 
 function App() {
+
+  useEffect(() => {
+    Axios.defaults.baseURL = import.meta.env.VITE_SERVER_ADDRESS;
+    Axios.defaults.withCredentials = true;
+
+    // Check if user is logged in
+    Axios.get('/')
+      .then((response) => {
+        console.log('User session:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching user session:', error);
+      });
+  }, []);
 
   return (
     <>
