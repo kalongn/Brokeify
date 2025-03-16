ed743f44a73f498ba66edbabe7790f38638311f2
Ka Long Ngai <63157613+kalongn@users.noreply.github.com>
2025-03-15 23:11:17 -0400

    [Frontend] Google OAuth frontend pre-req (#8)
    * Updated: basic Login page
    this page once implement should have those Aixos request working
    if session exist, redirect to Router page and therefore redirect the correct page
    if not wait until use Login or use the Continue as Guest
    
    * Updated: changed structure
    Login Page is now the Default Route
    /Home is the guest button and after sign in or redirect with session token
    NavBar and the overall page structure for every other page is very duplicate (probably there's a better solution but I can't think of one right now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,39 @@
 import { useEffect } from 'react';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import Axios from 'axios';
-
 import './App.css'
 
+import Login from './pages/Login.jsx';
 import Home from './pages/Home.jsx';
 
-function App() {
-
+const App = () => {
   useEffect(() => {
     Axios.defaults.baseURL = import.meta.env.VITE_SERVER_ADDRESS;
     Axios.defaults.withCredentials = true;
 
-    // Check if user is logged in
-    Axios.get('/')
-      .then((response) => {
-        console.log('User session:', response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching user session:', error);
-      });
+    // TODO: The Backend needs to be implemented to handle the session
+    // This is a placeholder for checking if the user is logged in
+    // Uncomment this when the backend is ready to handle sessions
+
+    // Axios.get('/')
+    //   .then((response) => {
+    //     console.log('User session:', response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching user session:', error);
+    //   });
   }, []);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </>
   )
 }
+
+//TODO: Add routing for the Pages through the Home Page (like redirection if the user already loggied in, or logged in with google OAuth, or continue with guest)
 
 export default App
