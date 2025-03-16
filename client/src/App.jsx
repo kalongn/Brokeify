9d38cac29fd779b6cd24c196f264c6a2cca762c1
Ka Long Ngai <kls8162@gmail.com>
2025-03-15 21:58:18 -0400

    Updated: basic Login page this page once implement should have those Aixos request working if session exist, redirect to Router page and therefore redirect the correct page if not wait until use Login or use the Continue as Guest

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
-import { Route, Routes } from "react-router-dom";
 import Axios from 'axios';
 
 import './App.css'
+import styles from './App.module.css';
 
-import Home from './pages/Home.jsx';
+// import Home from './pages/Home.jsx';
 
 function App() {
 
@@ -12,23 +12,50 @@ function App() {
     Axios.defaults.baseURL = import.meta.env.VITE_SERVER_ADDRESS;
     Axios.defaults.withCredentials = true;
 
-    // Check if user is logged in
-    Axios.get('/')
-      .then((response) => {
-        console.log('User session:', response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching user session:', error);
-      });
+    // TODO: The Backend needs to be implemented to handle the session
+    // This is a placeholder for checking if the user is logged in
+    // Uncomment this when the backend is ready to handle sessions
+    
+    // Axios.get('/')
+    //   .then((response) => {
+    //     console.log('User session:', response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching user session:', error);
+    //   });
   }, []);
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className={styles.left}>
+        <div className={styles.title}>
+          <div className={styles.icon}>
+            <img src='/icon.svg'></img>
+            <h2>Brokeify</h2>
+          </div>
+          <div className={styles.subtitle}>
+            <h1>Your Future,</h1>
+            <h1>Your Plan,</h1>
+            <h1 className={styles.important}>Our Guidence</h1>
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          <button className={`${styles.button} ${styles.googleButton}`} onClick={() => { console.log('Login Button') }}>
+            <img src='/src/assets/google.svg' alt='Google Icon' />
+            Login with Google
+          </button>
+          <button className={styles.button} onClick={() => { console.log('Register Button') }}>
+            Continue as Guest
+          </button>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <img src="/src/assets/3d-piggy-bank.svg" alt="3D Piggy Bank Rendering" />
+      </div>
     </>
   )
 }
+
+//TODO: Add routing for the Pages through the Home Page (like redirection if the user already loggied in, or logged in with google OAuth, or continue with guest)
 
 export default App
