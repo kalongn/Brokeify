5ff997ace6f739f7aa3bfee44629181305191809
sangeetavarri <sangeetavarri@gmail.com>
2025-03-16 20:48:58 -0400

    Made NavBar parts

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
 // TODO: to be actually implement then random stuff here
 
-// import style from './Navbar.module.css';
-// import { Link } from 'react-router-dom';
+import style from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <>
-        </>
+    <div className = {style.navbar}>
+        <h2>Brokeify</h2>
+
+        <button > Create Scenario </button>
+   
+       
+            <div className={style.navLinks}>
+                <Link to='/Home' className={style.navLink} onClick={() => { console.log('My Scenarios Page') }}>My Scenarios</Link>
+
+                <Link to='/Home'  className={style.navLink}  onClick={() => { console.log('Shared Scenarios Page') }}>Shared Scenarios</Link>
+            </div>
+
+            <div className={style.profileLink}>
+                
+                <Link to='/Home' className={style.profileLink} onClick={() => { console.log('Profile Page') }}>
+                    My Profile
+                </Link>
+            
+        </div>
+    </div>
     );
 }
 
