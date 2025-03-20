66b468370e23792ee687e4c3e2f44454b06c1c99
sangeetavarri <sangeetavarri@gmail.com>
2025-03-20 04:37:13 -0400

    Added CSS for header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import style from './Header.module.css';
 const Header = () => {
     return (
       // TODO: replace simple header placeholder
-        <header>
+        <header className= {style.header} >
             <h2>Placeholder Header</h2>
         </header>
     );
