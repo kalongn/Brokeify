ba5e83ad55ddcaf90cd292fa4022767229501b31
sangeetavarri <sangeetavarri@gmail.com>
2025-03-20 17:08:16 -0400

    Merged header code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,48 @@
-import style from './Header.module.css';
+import React from 'react';
+import styles from  './Header.module.css';
+import { useLocation } from 'react-router-dom';
+
 const Header = () => {
+    const location = useLocation();
+    const path = location.pathname;
+    const getHeaderTitle = () => {
+        switch(path) {
+            case '/Home':
+                return 'My Scenarios';
+            case '/ScenarioForm':
+                return 'Create/Edit Scenario';
+            case '/SharedScenarios':
+                return 'Shared Scenarios';
+            case '/Profile':
+                return 'My Profile';
+            default:
+                return 'Brokeify';
+        }
+    };
+    const getHeaderButtons = () => {
+        switch (path) {
+            case '/Home':
+                return (
+                    <>
+                    <button onClick={() => console.log('Import Scenario')}>Import Scenario</button>
+                    </>
+            );
+            case '/Profile':
+                return (
+                    <>
+                    <button onClick={() => console.log('Logout')}>Logout</button>
+                    </>
+            );
+            case '/SharedScenarios':
+            case '/ScenarioForm':
+            default:
+                return null;
+        }
+    };
     return (
-      // TODO: replace simple header placeholder
-        <header className= {style.header} >
-            <h2>Placeholder Header</h2>
+        <header className={styles.header}>
+            <h1>{getHeaderTitle()}</h1>
+            <div className={styles.buttons}>{getHeaderButtons()}</div>
         </header>
     );
 };
