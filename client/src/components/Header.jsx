9f1c69c2956cbf4fd527d2aa17f891b3114007a0
Michelle <michelleh11219@gmail.com>
2025-03-20 15:20:44 -0400

    adjust size and padding

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from  './Header.module.css';
 
 const Header = () => {
     return (
       // TODO: replace simple header placeholder
         <header>
-            <h2>Placeholder Header</h2>
+            <h1>Placeholder Header</h1>
         </header>
     );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 // TODO: to be actually implement then random stuff here
 
 import style from './NavBar.module.css';
+import iconStyle from '../pages/Login.module.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
     <div className = {style.navbar}>
-        <h2>Brokeify</h2>
+        <div className={`${iconStyle.icon} ${style.navIcon}`}>
+            <img src='/icon.svg'></img>
+            <h2 id={style.appName}>Brokeify</h2>
+        </div>
         <button> Create Scenario </button>
             <div className={style.navLinks}>
                 <Link to='/Home' className={style.navLink} onClick={() => { console.log('My Scenarios Page') }}>My Scenarios</Link>
@@ -16,7 +20,7 @@ const Navbar = () => {
 
             <div className={style.profileLink}>
                 
-                <Link to='/Profile' className={style.profileLink} onClick={() => { console.log('Profile Page') }}>
+                <Link to='/Profile' className={`${style.profileLink} ${style.navLink}`} onClick={() => { console.log('Profile Page') }}>
                     My Profile
                 </Link>
             
