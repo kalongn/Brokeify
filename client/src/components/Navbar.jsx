390f17b4c2c052d4cdd83171e59baf32ebece127
sangeetavarri <sangeetavarri@gmail.com>
2025-03-20 04:32:48 -0400

    Updated NavBar Design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,16 @@
 
 import style from './NavBar.module.css';
 import { Link } from 'react-router-dom';
+import { FaPlus } from "react-icons/fa6";
 
 const Navbar = () => {
     return (
     <div className = {style.navbar}>
-        <h2>Brokeify</h2>
-        <button> Create Scenario </button>
+        <div className={style.title}>
+        <img id = {style.iconImg} src = "icon.svg"></img>
+        <h2 id ={style.navTitle}> Brokeify</h2>
+        </div>
+         <button id = {style.createButton}><FaPlus />  Create Scenario </button>
             <div className={style.navLinks}>
                 <Link to='/Home' className={style.navLink} onClick={() => { console.log('My Scenarios Page') }}>My Scenarios</Link>
 
