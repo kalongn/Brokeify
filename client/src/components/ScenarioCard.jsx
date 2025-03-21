ec5a6caf8605e64bd3b335ba47f01245fd4d0fe6
Ka Long Ngai <63157613+kalongn@users.noreply.github.com>
2025-03-21 15:59:50 -0400

    [Frontend] dashboard (#18)
    * Made NavBar parts
    
    * Updated navbar to include profile page linking
    
    * Modified home and profile page (both in progress)
    
    * added ProfilePage as a Route in App.jsx
    
    * set up common layout
    
    * Updated: google oauth basic
    currently ensure user can login and out with the oauth
    ensure only 1 user is created / save to DB
    attached token to the user object
    need to work that in with express session storing into mongoDB
    need to ensure can load stuff from user profile.
    
    * Updated Profile Page
    
    * Added the profile pic- still needs to be modified
    
    * Updated CSS of profile page slightly
    
    * Modified: google oauth import settings.
    
    * Updated: persistent session.
    added ability to save session to DB as well.
    
    * Updated: reformatted code and new feature
    reformat code such that we have dedicate place for authentication and routing and not all in server.js
    index.js handle an extra profile selection to ensure the frontend can access the data.
    
    * Fixed a few naming errors - also merged oauth
    
    * Modified: make google port flexible to user define server port.
    
    * Patched: typo in Login page.
    
    * Modified: uncomment strict mode.
    
    * Updated icons
    
    * fix layout to use grid instead
    
    * Addressing a few comments
    
    * Adjusted for comments, also added shadowing effect for profile and fixed some structural features
    
    * Updated NavBar Design
    
    * Added CSS for header
    
    * Modified: identation and some minor changes to the display of root element.
    
    * Patched: Sizing issues on homepage.
    
    * Merged header code
    
    * Patched: Profile page css.
    
    * Patched: minor patch on profile.css
    
    * Pushed header to the top of the page
    
    * ScenarioCard In Progress
    
    * Scenario Card Done
    
    * Home page done
    
    * Removed an unused icon
    
    * Resolving merge errors
    
    * Merge conflicts
    
    * Commented out un-used imports
    
    * Added propTypes to pass ESLint
    
    * Reverted: changes to Login.module.css.
    
    * Updated Link / <a> tags updated.
    also updated the Logout button to actually works.
    
    * Modified: truncated package-lock.json on client.
    
    ---------
    
    Co-authored-by: sangeetavarri <sangeetavarri@gmail.com>
    Co-authored-by: Michelle <michelleh11219@gmail.com>

---
+++ client/src/components/ScenarioCard.jsx
@@ -0,0 +1,52 @@
+import styles from './ScenarioCard.module.css';
+import { IoPersonCircle } from "react-icons/io5";
+import { TbTargetArrow } from "react-icons/tb";
+import { IoMdCalendar } from "react-icons/io";
+import { FaPiggyBank } from "react-icons/fa6";
+
+import PropTypes from 'prop-types';
+
+const ScenarioCard = ({ title, martialStatus, targetAmount, investments, events }) => {
+  return (
+    <div className={styles.scenarioCard}>
+
+      <div className={styles.titleContainer}>
+        {/*Note: This is what's shown originally, when the user isn't hovering 
+        over the scenario card*/}
+        <div className={styles.title}>{title}</div>
+      </div>
+
+      <div className={styles.detailContainer}>
+          {/*Note: This info is visible over hover*/}
+          <div className={styles.detailHeader}>
+            <p className={styles.detailTitle}> {title} </p>
+          </div>
+          <div className = {styles.info}>
+          <div className={styles.infoRow}>
+            <IoPersonCircle size ={30}/> <span>{martialStatus}</span>
+          </div>
+          <div className={styles.infoRow}>
+            <TbTargetArrow size ={30} /> <span>{targetAmount?.toLocaleString() ?? "N/A"}</span>
+          </div>
+          <div className={styles.infoRow}>
+            <IoMdCalendar size ={30} /> <span>{investments} Investments</span>
+          </div>
+          <div className={styles.infoRow}>
+            <FaPiggyBank size ={30} /> <span>{events} Events</span>
+          </div>
+          </div> 
+      </div>
+    </div>
+  );
+};
+
+
+ScenarioCard.propTypes = {
+  title: PropTypes.string.isRequired, 
+  martialStatus: PropTypes.string,    
+  targetAmount: PropTypes.number,    
+  investments: PropTypes.number,       
+  events: PropTypes.number,           
+};
+
+export default ScenarioCard;
