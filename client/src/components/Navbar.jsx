87f068bd161417070d87174cf8b139ce660f1807
Michelle <michelleh11219@gmail.com>
2025-03-22 22:36:38 -0400

    Modified: relinked screnario form overridden in merge conflict resolution

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,28 +5,33 @@ import { Link } from 'react-router-dom';
 import { FaPlus } from "react-icons/fa6";
 
 const Navbar = () => {
-    return (
-        <div className={style.navbar}>
-            <div className={style.title}>
-                <img id={style.iconImg} src="icon.svg"></img>
-                <h2 id={style.navTitle}> Brokeify</h2>
-            </div>
-            <button id={style.createButton}><FaPlus />  Create Scenario </button>
-            <div className={style.navLinks}>
-                <Link to='/Home' className={style.navLink} onClick={() => { console.log('My Scenarios Page') }}>My Scenarios</Link>
+  return (
+    <div className={style.navbar}>
+      <div className={style.title}>
+        <img id={style.iconImg} src="icon.svg"></img>
+        <h2 id={style.navTitle}> Brokeify</h2>
+      </div>
+      <Link
+        id={style.createButton}
+        to="/ScenarioForm"
+        className={style.createButton}
+      >
+        <FaPlus /> Create Scenario
+      </Link>
+      <div className={style.navLinks}>
+        <Link to='/Home' className={style.navLink} onClick={() => { console.log('My Scenarios Page') }}>My Scenarios</Link>
+        <Link to='/SharedScenarios' className={style.navLink} onClick={() => { console.log('Shared Scenarios Page') }}>Shared Scenarios</Link>
+      </div>
 
-                <Link to='/Home' className={style.navLink} onClick={() => { console.log('Shared Scenarios Page') }}>Shared Scenarios</Link>
-            </div>
+      <div className={style.profileLink}>
 
-            <div className={style.profileLink}>
+        <Link to='/Profile' className={style.profileLink} onClick={() => { console.log('Profile Page') }}>
+          My Profile
+        </Link>
 
-                <Link to='/Profile' className={style.profileLink} onClick={() => { console.log('Profile Page') }}>
-                    My Profile
-                </Link>
-
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
