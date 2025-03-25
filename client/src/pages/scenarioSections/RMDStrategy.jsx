7cd644a70acc724e8c2dc1f05030bc75496421e9
Michelle <michelleh11219@gmail.com>
2025-03-24 20:34:40 -0400

    Updated: created strategies

--- client/src/pages/scenarioSections/RMDStrategy.jsx
+++ client/src/pages/scenarioSections/RMDStrategy.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import SortableList from "../../components/SortableList";
+
 const RMDStrategy = () => {
+  // TODO: pull from list of investments and reformat into below
+  // investments has name, value, expectedAnnualReturn, taxability
+  // name and value are from investment while expectedAnnualReturn and taxability are from investment type
+  // Keys must be named as id, amount, percentage, additional
+  const investments = [
+    { id: "Investment Withdrawal 1", amount: `$100`, percentage: `1.9% return`, additional: `Taxable` },
+    { id: "Investment Withdrawal 2", amount: `$5000`, percentage: `4% return`, additional: `Taxable` },
+    { id: "Investment Withdrawal 3", amount: `$50`, percentage: `9% return`, additional: `Tax-exempt` },
+  ];
+  const [strategy, setStrategy] = useState([investments]);
+  const handleReorder = (list) => {
+    setStrategy(list);
+    if(!strategy) {
+      console.log("no strategy");
+    }
+  };
+  // TODO: save rmd strategy to db
+
   return (
     <div>
       <h2>Required Minimum Distribution Strategy</h2>
@@ -7,8 +28,10 @@ const RMDStrategy = () => {
         from pre-tax retirement accounts to non-retirement accounts
         when a Required Minimum Distribution (RMD) is triggered.
       </p>
-      {/* TODO: replace this with actual sortable component */}
-      <div>list item 1</div>
+      <SortableList
+        items={investments}
+        handleReorder={handleReorder}
+      />
     </div>
   );
 };
--- client/src/pages/scenarioSections/SpendingStrategy.jsx
+++ client/src/pages/scenarioSections/SpendingStrategy.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import SortableList from "../../components/SortableList";
+
 const SpendingStrategy = () => {
+  // TODO: pull from list of discretionary expenses and reformat into below
+  // expenses has name, amount, userContributions, spouseContributions
+  // Keys must be named as id, amount, percentage, additional
+  const expenses = [
+    { id: "Discretionary Expense 1", amount: `$100`, percentage: `1.9% increase`, additional: `0% increase` },
+    { id: "Discretionary Expense 2", amount: `$5000`, percentage: `4% increase`, additional: `3% increase` },
+    { id: "Discretionary Expense 3", amount: `$50`, percentage: `9% increase`, additional: `5% increase` },
+    { id: "Discretionary Expense 4", amount: `$400`, percentage: `8.4% increase`, additional: `3.7% increase` },
+    { id: "Discretionary Expense 5", amount: `$899`, percentage: `0% increase`, additional: `1.4% increase` },
+  ];
+  const [strategy, setStrategy] = useState([expenses]);
+  const handleReorder = (list) => {
+    setStrategy(list);
+    if(!strategy) {
+      console.log("no strategy");
+    }
+  };
+  // TODO: save spending strategy to db
+
   return (
     <div>
       <h2>Spending Strategy</h2>
       <p>
         Specify the order of discretionary expenses to be paid as cash allows.
       </p>
-      {/* TODO: replace this with actual sortable component */}
-      <div>list item 1</div>
+      <SortableList
+        items={expenses}
+        handleReorder={handleReorder}
+      />
     </div>
   );
 };
