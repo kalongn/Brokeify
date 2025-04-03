c876ba6648bfab2f094ffa997bd72a109473789f
Ka Long Ngai <kls8162@gmail.com>
2025-04-03 19:59:22 -0400

    Modified: corrected typo on sparse option

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -5,8 +5,8 @@ import { PERMISSION } from './Enums.js';
 const UserSchema = new mongoose.Schema({
     firstName: { type: String, default: 'Guest' },
     lastName: { type: String, default: 'Guest' },
-    email: { type: String, unique: true, sparsed: true },
-    googleId: { type: String, unique: true, sparsed: true },
+    email: { type: String, unique: true, sparse: true, default: undefined },
+    googleId: { type: String, unique: true, sparse: true, default: undefined },
     picture: { type: String },
     refreshToken: { type: String },
     accessToken: { type: String },
