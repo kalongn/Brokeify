e56f9b5f0cf3ae464791cafd61ac5faeb2c32d2b
Ka Long Ngai <kls8162@gmail.com>
2025-04-05 12:14:36 -0400

    Modified: limits route relocated inflation distribution / pretax limit

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,15 +9,7 @@ import basicInfoRoutes from './basicInfoRoutes.js';
 import investmentTypesRoutes from './investmentTypesRoutes.js';
 import investmentsRoutes from './investmentsRoutes.js';
 import eventsRoutes from './eventsRoutes.js';
-
-import ScenarioController from '../db/controllers/ScenarioController.js';
-import DistributionController from '../db/controllers/DistributionController.js';
-
-import {
-    distributionToFrontend,
-    distributionToBackend,
-    canEdit,
-} from './helper.js';
+import limitsRoutes from './limitsRoutes.js';
 
 const router = express.Router();
 router.use(authRoutes);
@@ -29,10 +21,7 @@ router.use(basicInfoRoutes);
 router.use(investmentTypesRoutes);
 router.use(investmentsRoutes);
 router.use(eventsRoutes);
-
-const scenarioController = new ScenarioController();
-const distributionController = new DistributionController();
-
+router.use(limitsRoutes);
 
 router.get("/", async (req, res) => {
     console.log(req.session); // can be used to debug session data
@@ -40,66 +29,6 @@ router.get("/", async (req, res) => {
         return res.status(200).send("Verified, userId: " + req.session.user);
     } else {
         return res.status(204).send();
-    }
-});
-
-
-router.get("/limits/:scenarioId", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send("Not logged in.");
-    }
-    try {
-        const userId = req.session.user;
-        const id = req.params.scenarioId;
-        if (!await canEdit(userId, id)) {
-            return res.status(403).send("You do not have permission to access this scenario.");
-        }
-
-        const scenario = await scenarioController.readWithPopulate(id);
-        const limits = {
-            inflationAssumptionDistribution: distributionToFrontend(scenario.inflationAssumptionDistribution),
-            annualPostTaxContributionLimit: scenario.annualPostTaxContributionLimit,
-        }
-        return res.status(200).send(limits);
-    } catch (error) {
-        console.error("Error in limits route:", error);
-        return res.status(500).send("Error retrieving limits.");
-    }
-});
-
-router.post("/limits/:scenarioId", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send("Not logged in.");
-    }
-    try {
-        const userId = req.session.user;
-        const id = req.params.scenarioId;
-        if (!await canEdit(userId, id)) {
-            return res.status(403).send("You do not have permission to access this scenario.");
-        }
-
-        const { initialLimit, inflationAssumption } = req.body;
-
-        const requestInflationAssumption = distributionToBackend(inflationAssumption);
-
-        const currentScenario = await scenarioController.read(id);
-        let updatedDistribution = null;
-        if (currentScenario.inflationAssumptionDistribution) {
-            updatedDistribution = await distributionController.update(currentScenario.inflationAssumptionDistribution, requestInflationAssumption);
-        } else {
-            const { distributionType, ...data } = requestInflationAssumption;
-            updatedDistribution = await distributionController.create(distributionType, data);
-        }
-
-        await scenarioController.update(id, {
-            annualPostTaxContributionLimit: initialLimit,
-            inflationAssumptionDistribution: updatedDistribution
-        });
-
-        return res.status(200).send("Limits updated.");
-    } catch (error) {
-        console.error("Error in limits route:", error);
-        return res.status(500).send("Error updating limits.");
     }
 });
 
