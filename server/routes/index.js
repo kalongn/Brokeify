e407c2b488b7017297ce1ca85c8f1fc2c4885aad
Ka Long Ngai <kls8162@gmail.com>
2025-04-04 22:48:48 -0400

    Modified: basicInfoRoute file created

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ import homeRoutes from './homeRoutes.js';
 import viewScenarioRoutes from './viewScenarioRoutes.js';
 import profileRoutes from './profileRoutes.js';
 import createScenarioRoutes from './createScenarioRoutes.js';
+import basicInfoRoutes from './basicInfoRoutes.js';
 
 import ScenarioController from '../db/controllers/ScenarioController.js';
 import InvestmentTypeController from '../db/controllers/InvestmentTypeController.js';
@@ -28,6 +29,7 @@ router.use(homeRoutes);
 router.use(viewScenarioRoutes);
 router.use(profileRoutes);
 router.use(createScenarioRoutes);
+router.use(basicInfoRoutes);
 
 const scenarioController = new ScenarioController();
 const investmentTypeController = new InvestmentTypeController();
@@ -42,103 +44,6 @@ router.get("/", async (req, res) => {
         return res.status(200).send("Verified, userId: " + req.session.user);
     } else {
         return res.status(204).send();
-    }
-});
-
-// obtain the basic info of the scenario
-router.get("/basicInfo/:scenarioId", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send("Not logged in.");
-    }
-    try {
-        const userId = req.session.user;
-        const id = req.params.scenarioId;
-        if (!canEdit(userId, id)) {
-            return res.status(403).send("You do not have permission to access this scenario.");
-        }
-        const scenario = await scenarioController.read(id);
-        await scenario.populate("userLifeExpectancyDistribution spouseLifeExpectancyDistribution");
-
-        const respondLifeExpectancy = distributionToFrontend(scenario.userLifeExpectancyDistribution);
-        const respondSpouseLifeExpectancy = distributionToFrontend(scenario.spouseLifeExpectancyDistribution);
-
-        const respondObject = {
-            name: scenario.name || null,
-            financialGoal: scenario.financialGoal || null,
-            state: scenario.stateOfResidence || null,
-            maritalStatus: scenario.filingStatus || null,
-            birthYear: scenario.userBirthYear || null,
-            lifeExpectancy: respondLifeExpectancy,
-            spouseBirthYear: scenario.spouseBirthYear || null,
-            spouseLifeExpectancy: respondSpouseLifeExpectancy
-        }
-
-        return res.status(200).send(respondObject);
-    } catch (error) {
-        console.error("Error in basic info route:", error);
-        return res.status(500).send("Error retrieving basic info.");
-    }
-});
-
-// update the basic info of the scenario, (created if part of the basic info is missing)
-router.post("/basicInfo/:scenarioId", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).send("Not logged in.");
-    }
-
-    try {
-        const userId = req.session.user;
-        const id = req.params.scenarioId;
-        if (!await canEdit(userId, id)) {
-            return res.status(403).send("You do not have permission to access this scenario.");
-        }
-
-        const { name, financialGoal, state, maritalStatus, birthYear, lifeExpectancy, spouseBirthYear, spouseLifeExpectancy } = req.body;
-        const currentScenario = await scenarioController.read(id);
-
-        if (!currentScenario) {
-            // Should not happen, but just in case
-            return res.status(404).send("Scenario not found.");
-        }
-
-        const requestLifeExpectancy = frontendToDistribution(lifeExpectancy);
-        const requestSpouseLifeExpectancy = frontendToDistribution(spouseLifeExpectancy);
-
-        let newUserLifeExpectancy = null;
-        if (currentScenario.userLifeExpectancyDistribution) {
-            newUserLifeExpectancy = await distributionController.update(currentScenario.userLifeExpectancyDistribution, requestLifeExpectancy);
-        } else {
-            const { distributionType, ...data } = requestLifeExpectancy;
-            newUserLifeExpectancy = await distributionController.create(distributionType, data);
-        }
-
-        let spouseLifeExpectancyDistribution = null;
-        if (!requestSpouseLifeExpectancy && currentScenario.spouseLifeExpectancyDistribution) {
-            await distributionController.delete(currentScenario.spouseLifeExpectancyDistribution);
-        } else if (requestSpouseLifeExpectancy) {
-            if (!currentScenario.spouseLifeExpectancyDistribution) {
-                const { distributionType, ...data } = requestSpouseLifeExpectancy;
-                spouseLifeExpectancyDistribution = await distributionController.create(distributionType, data);
-            } else {
-                spouseLifeExpectancyDistribution = await distributionController.update(currentScenario.spouseLifeExpectancyDistribution, requestSpouseLifeExpectancy);
-            }
-        }
-
-        await scenarioController.update(id, {
-            name: name,
-            financialGoal: financialGoal,
-            stateOfResidence: state,
-            filingStatus: maritalStatus,
-            userBirthYear: birthYear,
-            userLifeExpectancyDistribution: newUserLifeExpectancy,
-            spouseBirthYear: spouseBirthYear === undefined ? null : spouseBirthYear,
-            spouseLifeExpectancyDistribution: spouseLifeExpectancyDistribution
-        });
-
-        return res.status(200).send("Basic info updated.");
-    } catch (error) {
-        console.error("Error in basic info route:", error);
-        return res.status(500).send("Error updating basic info.");
     }
 });
 
