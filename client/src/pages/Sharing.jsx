0f4aefac0577310e67348e0cef280c55535a3171
Ka Long Ngai <kls8162@gmail.com>
2025-04-09 18:24:30 -0400

    Modified: checking for duplicate email the localname part become case insensitive the domain name part remain case sensitive

--- client/src/pages/Sharing.jsx
+++ client/src/pages/Sharing.jsx
@@ -91,7 +91,11 @@ const Sharing = () => {
       setErrors("Invalid email format");
       return;
     }
-    if (sharedUsers.some((user) => user.email === email)) {
+    if (sharedUsers.some((user) => {
+      const [local, domain] = user.email.split('@');
+      const [localInput, domainInput] = email.split('@');
+      return local.toLowerCase() === localInput.toLowerCase() && domain === domainInput;
+    })) {
       setErrors("User already added");
       return;
     }
