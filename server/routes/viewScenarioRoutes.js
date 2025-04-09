178d50cacf49394f0ae9835448e375869a74af1e
Ka Long Ngai <kls8162@gmail.com>
2025-04-08 23:19:02 -0400

    Modified: ViewScenario frontend logic moved to backend

--- server/routes/viewScenarioRoutes.js
+++ server/routes/viewScenarioRoutes.js
@@ -6,7 +6,6 @@ import { canView, distributionToString, stateAbbreviationToString, taxStatusToFr
 const router = express.Router();
 const scenarioController = new ScenarioController();
 
-
 router.get("/scenario/:scenarioId", async (req, res) => {
     if (req.session.user) {
         try {
@@ -85,19 +84,101 @@ router.get("/scenario/:scenarioId", async (req, res) => {
 
             const data = {
                 name: scenario.name,
+                financialGoal: scenario.financialGoal,
+                filingStatus: scenario.filingStatus,
+                userBirthYear: scenario.userBirthYear,
+                userLifeExpectancyDistribution: distributionToString(scenario.userLifeExpectancyDistribution),
+                stateOfResidence: stateAbbreviationToString(scenario.stateOfResidence),
+                investments: investments,
+                events: events,
+                orderedSpendingStrategy: spendingStrategy,
+                orderedExpenseWithdrawalStrategy: expenseWithdrawalStrategy,
+                orderedRMDStrategy: rmdStrategy,
+                startYearRothOptimizer: scenario.startYearRothOptimizer,
+                orderedRothStrategy: rothStrategy,
+            }
+
+            return res.status(200).send(data);
+        } catch (error) {
+            console.error("Error in scenario route:", error);
+            return res.status(500).send("Error retrieving scenario.");
+        }
+    } else {
+        res.status(401).send("Not logged in.");
+    }
+});
+
+router.get("/scenario-detail/:scenarioId", async (req, res) => {
+    if (req.session.user) {
+        try {
+            const id = req.params.scenarioId;
+
+            if (!await canView(req.session.user, id)) {
+                return res.status(403).send("You do not have permission to access this scenario.");
+            }
+
+            let scenario = await scenarioController.readWithPopulate(id);
+            if (!scenario) {
+                return res.status(404).send("Scenario not found.");
+            }
+
+            const investments = [];
+            const investmentIdMap = {};
+            for (let investmentType of scenario.investmentTypes) {
+                for (let investment of investmentType.investments) {
+                    const investmentStructure = {
+                        name: investmentType.name,
+                        value: investment.value,
+                        taxStatus: investment.taxStatus,
+                    }
+                    investmentIdMap[investment._id] = investmentStructure;
+                    investments.push(investmentStructure);
+                }
+            }
+
+            const events = []
+            const eventIdMap = {};
+            for (let event of scenario.events) {
+                const eventStructure = {
+                    name: event.name,
+                    type: event.eventType
+                }
+                eventIdMap[event._id] = eventStructure;
+                events.push(eventStructure);
+            }
+
+            const spendingStrategy = scenario.orderedSpendingStrategy.map(eventId => {
+                return eventIdMap[eventId];
+            });
+
+            const expenseWithdrawalStrategy = scenario.orderedExpenseWithdrawalStrategy.map(investId => {
+                return investmentIdMap[investId];
+            });
+
+            const rmdStrategy = scenario.orderedRMDStrategy.map(investId => {
+                return investmentIdMap[investId];
+            });
+
+            const rothStrategy = scenario.orderedRothStrategy.map(investId => {
+                return investmentIdMap[investId];
+            });
+
+
+            const data = {
+                name: scenario.name,
                 ownerFirstName: scenario.ownerFirstName,
                 ownerLastName: scenario.ownerLastName,
                 financialGoal: scenario.financialGoal,
                 filingStatus: scenario.filingStatus,
                 userBirthYear: scenario.userBirthYear,
                 spouseBirthYear: scenario.spouseBirthYear,
-                userLifeExpectancyDistribution: distributionToString(scenario.userLifeExpectancyDistribution),
-                spouseLifeExpectancyDistribution: distributionToString(scenario.spouseLifeExpectancyDistribution),
+                userLifeExpectancyDistribution: scenario.userLifeExpectancyDistribution,
+                spouseLifeExpectancyDistribution: scenario.spouseLifeExpectancyDistribution,
                 stateOfResidence: stateAbbreviationToString(scenario.stateOfResidence),
                 investments: investments,
                 events: events,
-                inflationAssumptionDistribution: distributionToString(scenario.inflationAssumptionDistribution),
-                annualPostTaxContributionLimit: distributionToString(scenario.annualPostTaxContributionLimit),
+                inflationAssumptionDistribution: scenario.inflationAssumptionDistribution,
+                annualPostTaxContributionLimit: scenario.annualPostTaxContributionLimit,
                 orderedSpendingStrategy: spendingStrategy,
                 orderedExpenseWithdrawalStrategy: expenseWithdrawalStrategy,
                 orderedRMDStrategy: rmdStrategy,
