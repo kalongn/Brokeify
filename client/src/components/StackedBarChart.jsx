fc427148dea5e7b74bf5262772ebfaea46d9f44a
sangeetavarri <sangeetavarri@gmail.com>
2025-04-09 23:00:17 -0400

    Removed unecessary comments

--- client/src/components/StackedBarChart.jsx
+++ client/src/components/StackedBarChart.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 
 const StackedBarChart = ({ data }) => {
-    // Get all keys except 'labels'
+    
     const investmentKeys = Object.keys(data).filter(key => key !== 'labels');
 
     const plotData = investmentKeys.map((key) => ({
         x: data.labels,
         y: data[key],
         type: 'bar',
-        name: key, // Use the key directly as the name
+        name: key,
     }));
 
     return (
@@ -19,7 +19,7 @@ const StackedBarChart = ({ data }) => {
                 title: 'Stacked Bar Chart',
                 barmode: 'stack',
                 xaxis: { title: 'Time' },
-                yaxis: { title: 'Amount' },
+                yasxis: { title: 'Amount' },
             }}
         />
     );
