fa1183d7811d9327284ca1e408f6a1452bb0570d
Ka Long Ngai <kls8162@gmail.com>
2025-04-09 22:07:09 -0400

    Modified: added a guest -> user convertion hint on the sharedScenarios.jsx page

--- client/src/pages/SharedScenarios.jsx
+++ client/src/pages/SharedScenarios.jsx
@@ -1,3 +1,5 @@
+// TODO: Make guest able to see this page but replace with a message that they need to create an account to use this.
+
 import Layout from "../components/Layout";
 const SharedScenarios = () => {
   return (
