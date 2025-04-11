479c598eda3f682ac213cb774502d8f7cf2f5519
04mHuang <71652653+04mHuang@users.noreply.github.com>
2025-04-11 13:42:00 -0400

    [Frontend] [Tech Debt] Header adjusted (#64)
    Adjusted Header to output correct things.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,10 +8,15 @@ const Header = ({ setVerified }) => {
   const location = useLocation();
   const path = location.pathname;
   const getHeaderTitle = () => {
+    // Some paths have scenario ID, so need to check with startsWith
+    if (path.startsWith('/ScenarioForm')) {
+      return 'Create/Edit Scenario';
+    }
 
     if (path.startsWith('/Scenario')) {
       return 'Scenario Simulation';
     }
+
     if (path.startsWith('/ViewScenario')) {
       return 'View Scenario';
     }
@@ -27,93 +32,58 @@ const Header = ({ setVerified }) => {
     switch (path) {
       case '/Home':
         return 'My Scenarios';
-      case '/ScenarioForm':
-        return 'Create/Edit Scenario';
       case '/SharedScenarios':
         return 'Shared Scenarios';
       case '/Profile':
         return 'My Profile';
-      case '/Scenario':
-        return 'Scenario Simulation';
-      case '/ViewScenario':
-        return 'View Scenario';
-      
       default:
         return 'Brokeify';
     }
   };
   // TODO: implement the IMPORT SCENARIO button functionality
   const getHeaderButtons = () => {
-
-    if (path.startsWith('/Scenario')) {
-
+    // Scenario simulation page
+    if (path.startsWith('/Scenario/')) {
       return (
-        <>
-          <div className={styles.buttonGroupSimulation}>
-            <button onClick={() => console.log('Export Scenario')}>Export </button>
-          </div>
-        </>
+        <div className={styles.buttonGroupSimulation}>
+          <button onClick={() => console.log('Share Scenario')}>Share </button>
+          <button onClick={() => console.log('Export Scenario')}>Export </button>
+        </div>
       );
     }
+    // View scenario page
     if (path.startsWith('/ViewScenario')) {
       const pathParts = path.split('/');
       const id = pathParts[pathParts.length - 1];
       return (
-        <>
-          <Link to={`/Scenario/${id}`} className={styles.icon}><VscChromeClose /></Link>
-        </>
+        <Link to={`/Scenario/${id}`} className={styles.icon}><VscChromeClose /></Link>
       );
     }
+    // Sharing page
     if (path.startsWith('/Sharing')) {
       const pathParts = path.split('/');
       const id = pathParts[pathParts.length - 1];
       return (
-        <>
-          <Link to={`/Scenario/${id}`} className={styles.icon}><VscChromeClose /></Link>
-        </>
+        <Link to={`/Scenario/${id}`} className={styles.icon}><VscChromeClose /></Link>
       );
     }
-
+    // Charts page
     if (path.startsWith('/Visualizations/Charts')) {
       const pathParts = path.split('/');
       const id = pathParts[pathParts.length - 1];
       return (
-        <>
-          <Link to={`/Scenario/${id}`} className={styles.icon}><VscChromeClose /></Link>
-        </>
+        <Link to={`/Scenario/${id}`} className={styles.icon}><VscChromeClose /></Link>
       );
     }
-
 
     switch (path) {
       case '/Home':
         return (
-          <>
-            <button onClick={() => console.log('Import Scenario')}>Import Scenario</button>
-          </>
+          <button onClick={() => console.log('Import Scenario')}>Import Scenario</button>
         );
       case '/Profile':
         return (
-          <>
-            <Link onClick={() => setVerified(false)} className={styles.linkButton} to={`${import.meta.env.VITE_SERVER_ADDRESS}/logout`}>Logout</Link>
-          </>
-        );
-      case '/SharedScenarios':
-      case '/ScenarioForm':
-      case '/Scenario':
-        return (
-          <>
-            <div className={styles.buttonGroupSimulation}>
-              <button onClick={() => console.log('Share Scenario')}>Share </button>
-              <button onClick={() => console.log('Export Scenario')}>Export </button>
-            </div>
-          </>
-        );
-      case '/ViewScenario':
-        return (
-          <>
-            <Link to='/Scenario' className={styles.icon}><VscChromeClose /></Link>
-          </>
+          <Link onClick={() => setVerified(false)} className={styles.linkButton} to={`${import.meta.env.VITE_SERVER_ADDRESS}/logout`}>Logout</Link>
         );
       default:
         return null;
