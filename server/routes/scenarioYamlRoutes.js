a7e12c3457c4be5851130cfbc4f84b33e3012ab3
Ka Long Ngai <kls8162@gmail.com>
2025-04-12 20:47:36 -0400

    UNSTABLE updated: a theoritical router handler

--- server/routes/scenarioYamlRoutes.js
+++ server/routes/scenarioYamlRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import multer from 'multer';
+import yaml from 'js-yaml';
+import fs from 'fs';
+
 import { exportScenarioAsYAML } from '../yaml_parsers/scenarioExporter.js';
+import { parseAndSaveYAML } from '../yaml_parsers/scenarioParser.js';
 import { canView } from './helper.js';
 
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/scenario/:scenarioId/export', async (req, res) => {
     if (!req.session.user) {
@@ -21,6 +27,29 @@ router.get('/scenario/:scenarioId/export', async (req, res) => {
     } catch (error) {
         console.error("Error exporting scenario as YAML:", error);
         res.status(500).send("Error exporting scenario.");
+    }
+});
+
+router.post(('/scenario/import'), upload.single('file'), async (req, res) => {
+    if (!req.session.user) {
+        return res.status(401).send("Not logged in.");
+    }
+    try {
+        const filePath = req.file.path; // Path to the uploaded file
+        const fileContent = fs.readFileSync(filePath, 'utf8'); // Read the file content
+        const yamlStr = yaml.load(fileContent);
+        fs.unlinkSync(filePath);
+
+        console.log("YAML content:", yamlStr); // For debugging purposes
+
+        const scenarioId = await parseAndSaveYAML(yamlStr, req.session.user);
+        res.status(200).send({
+            message: "Scenario imported successfully.",
+            scenarioId: scenarioId
+        });
+    } catch (error) {
+        console.error("Error importing scenario from YAML:", error);
+        res.status(500).send("Error importing scenario.");
     }
 });
 
