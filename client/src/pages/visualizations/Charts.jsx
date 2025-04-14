e61d081d0217b97a235a27390c5eda739e94ae3f
Ka Long Ngai <kls8162@gmail.com>
2025-04-13 22:13:27 -0400

    Updated: Shaded line chart

--- client/src/pages/visualizations/Charts.jsx
+++ client/src/pages/visualizations/Charts.jsx
@@ -23,26 +23,8 @@ const Charts = () => {
     Axios.defaults.withCredentials = true;
   }, []);
 
-  const ShadedLineData = {
-    labels: ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'],
-    median: [0.5, 0.6, 0.55, 0.7, 0.65, 0.6, 0.75, 0.8, 0.85, 0.9],
-    lower10: [0.4, 0.5, 0.45, 0.6, 0.55, 0.5, 0.65, 0.7, 0.75, 0.8],
-    upper10: [0.6, 0.7, 0.65, 0.8, 0.75, 0.7, 0.85, 0.9, 0.95, 1.0],
-    lower20: [0.35, 0.45, 0.4, 0.55, 0.5, 0.45, 0.6, 0.65, 0.7, 0.75],
-    upper20: [0.65, 0.75, 0.7, 0.85, 0.8, 0.75, 0.9, 0.95, 1.0, 1.05],
-    lower30: [0.3, 0.4, 0.35, 0.5, 0.45, 0.4, 0.55, 0.6, 0.65, 0.7],
-    upper30: [0.7, 0.8, 0.75, 0.9, 0.85, 0.8, 0.95, 1.0, 1.05, 1.1],
-    lower40: [0.25, 0.35, 0.3, 0.45, 0.4, 0.35, 0.5, 0.55, 0.6, 0.65],
-    upper40: [0.75, 0.85, 0.8, 0.95, 0.9, 0.85, 1.0, 1.05, 1.1, 1.15],
-  };
 
   const [charts, setCharts] = useState([
-    // {
-    //   id: 1, type: "Line Chart", label: "Probability of Success over Time", data: {
-    //     labels: ['January', 'February', 'March', 'April', 'May'],
-    //     values: [0.2, 0.3, 0.5, 0.6, 0.7],
-    //   },
-    // },
     // {
     //   id: 2, type: "Stacked Bar Chart", label: "Total Investments (Median)", data: {
     //     labels: ['January', 'February', 'March', 'April', 'May'],
@@ -51,8 +33,7 @@ const Charts = () => {
     //     investments3: [150, 200, 250, 300, 350],
     //     investments4: [200, 250, 300, 350],
     //   },
-    // },
-    // { id: 3, type: "Shaded Line Data", label: "Investments", data: ShadedLineData },
+    // }
   ]);
 
 
@@ -110,7 +91,7 @@ const Charts = () => {
             <div key={chart.id} className={styles.chart}>
               <h3>{chart.type}</h3>
               {/* Charts will show depending on type */}
-              {chart.type === "Shaded Line Data" && <ShadedLineChart data={chart.data} />}
+              {chart.type === "Shaded Line Chart" && <ShadedLineChart data={chart.data} />}
               {chart.type === "Line Chart" && <LineChart data={chart.data} />}
               {chart.type === "Stacked Bar Chart" && <StackedBarChart data={chart.data} />}
             </div>
--- server/routes/viewResultRoutes.js
+++ server/routes/viewResultRoutes.js
@@ -31,6 +31,82 @@ const generateLineChartData = (yearToResults) => {
     }
 };
 
+const getPercentile = (arr, percentile) => {
+    const index = Math.floor(percentile * arr.length);
+    return arr[index];
+}
+
+const generateShadedLineData = (chart, yearToResults) => {
+    const labels = [];
+    const median = [];
+    const lower10 = [];
+    const upper10 = [];
+    const lower20 = [];
+    const upper20 = [];
+    const lower30 = [];
+    const upper30 = [];
+    const lower40 = [];
+    const upper40 = [];
+
+    for (const year in yearToResults) {
+        labels.push(year);
+        const yearlyResults = yearToResults[year];
+        const values = yearlyResults.map((result) => {
+            let value = 0;
+            switch (chart.content.quantity) {
+                case "Total Investments":
+                    value = result.investmentValues.reduce((acc, investment) => acc + investment.value, 0);
+                    break;
+                case "Total Income":
+                    value = result.totalIncome;
+                    break;
+                case "Total Expenses (including taxes)":
+                    value = result.totalExpense + result.totalTax;
+                    break;
+                case "Early Withdrawal Tax":
+                    value = result.earlyWithdrawalTax;
+                    break;
+                case "Percentage of Discretionary Expenses":
+                    value = result.totalDiscretionaryExpenses;
+                    break;
+                default:
+                    // Should not happen
+                    break;
+            }
+            if (chart.content.quantity !== "Percentage of Discretionary Expenses") {
+                if (chart.content.dollarValue === "Today") {
+                    value /= (1 + result.cumulativeInflation);
+                }
+            }
+            return value;
+        });
+        values.sort((a, b) => a - b);
+
+        median.push(getPercentile(values, 0.5));
+        lower10.push(getPercentile(values, 0.1));
+        upper10.push(getPercentile(values, 0.9));
+        lower20.push(getPercentile(values, 0.2));
+        upper20.push(getPercentile(values, 0.8));
+        lower30.push(getPercentile(values, 0.3));
+        upper30.push(getPercentile(values, 0.7));
+        lower40.push(getPercentile(values, 0.4));
+        upper40.push(getPercentile(values, 0.6));
+    }
+
+    return {
+        labels: labels,
+        median: median,
+        lower10: lower10,
+        upper10: upper10,
+        lower20: lower20,
+        upper20: upper20,
+        lower30: lower30,
+        upper30: upper30,
+        lower40: lower40,
+        upper40: upper40,
+    }
+};
+
 
 
 router.post("/charts/:simulationId", async (req, res) => {
@@ -51,17 +127,15 @@ router.post("/charts/:simulationId", async (req, res) => {
         });
     });
 
+
+
     charts.forEach((chart) => {
         switch (chart.type) {
             case "Line Chart":
                 chart.data = generateLineChartData(yearToResults);
                 break;
-            case "Shaded Line Data":
-                chart.data = {
-                    type: "shadedLine",
-                    data: chart.data,
-                    label: chart.label,
-                };
+            case "Shaded Line Chart":
+                chart.data = generateShadedLineData(chart, yearToResults);
                 break;
             case "Stacked Bar Chart":
                 chart.data = {
