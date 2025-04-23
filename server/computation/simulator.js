a59b4c3b13ed60e660dcc9cf97275a619c21fe92
Adrian Heath <adrian.heath@stonybrook.edu>
2025-04-23 11:27:32 -0400

    fix?

--- server/computation/simulator.js
+++ server/computation/simulator.js
@@ -397,15 +397,11 @@ export async function simulate(
         //update events
         for (const i in scenario.events) {
           const event = await eventFactory.read(scenario.events[i]);
-          if (event.eventType === "INCOME" || event.eventType === "EXPENSE") {
-            //if spouse was 100%, remove it completley
-            if (event.userContributions === 0) {
-              await eventFactory.delete(event._id);
-            } else {
-              await eventFactory.update(event._id, {
-                amount: event.amount * event.userContributions,
-              });
-            }
+          if (event.eventType === "INCOME" || event.eventType === "EXPENSE") {     
+            await eventFactory.update(event._id, {
+              amount: event.amount * event.userContributions,
+            });
+            
           }
         }
 
