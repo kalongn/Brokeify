8f20dcd08849344af6ccee6b5f1e31082916bc68
Ka Long Ngai <kls8162@gmail.com>
2025-04-28 17:09:53 -0400

    Updated: prevent exporting scenario at least need to fill up to basicInfo to be export

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -73,8 +73,16 @@ const Header = ({ setVerified }) => {
             a.click();
             a.remove();
           } catch (error) {
-            console.error('Error downloading file:', error);
-            alert('Error downloading file. Please try again.');
+            if (error.response && error.response?.status === 409) {
+              console.error('Scenario not filled out up to basic requirements.');
+              alert('Scenario not filled out up to basic requirements.');
+            } else if (error.response && error.response?.status === 403) {
+              console.error('You do not have permission to access this scenario.');
+              alert('You do not have permission to access this scenario.');
+            } else {
+              console.error('Error downloading file:', error);
+              alert('Error downloading file. Please try again.');
+            }
           }
         }}>Export Scenario</button>
       );
