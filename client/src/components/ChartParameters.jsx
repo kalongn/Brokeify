bc853672e1235269068bc98308e61995f8a6a553
Ka Long Ngai <kls8162@gmail.com>
2025-04-29 23:04:17 -0400

    Updated: load correct options events base on selected scenario

--- client/src/components/ChartParameters.jsx
+++ client/src/components/ChartParameters.jsx
@@ -1,18 +1,109 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
 import Select from 'react-select';
+import Axios from 'axios';
+
 import styles from './ChartTabs.module.css';
 
-const ChartParameters = ({ parameterIndex, selectRemount, simulationInput, handleChange, handleSelectChange }) => {
-  const parameterOptions = [
-    { value: "Roth", label: "Disable Roth Optimizer" },
+const ChartParameters = ({ parameterIndex, selectRemount, simulationInput, handleChange, handleSelectChange, setSelectRemount, setSimulationInput }) => {
+  const [parameterOptions, setparameterOptions] = useState([
     { value: "Start Year", label: "Start Year" },
     { value: "Duration", label: "Duration" },
     { value: "Initial Amount", label: "Initial Amount" },
-    { value: "Investment Percentage", label: "First of Two Investments" },
-  ];
-  const [eventSeries, setEventSeries] = useState([{ name: "Event 1" }, { name: "Event 2" }, { name: "Event 3" }]);
+    { value: "Investment Percentage", label: "First of Two Investments" }
+  ]);
+
+  const [events, setEvents] = useState([]);
+  const [incomeExpenseEvents, setincomeExpenseEvents] = useState([]);
+  const [investEvents, setInvestEvents] = useState([]);
+  const [displayedEvents, setdisplayedEvents] = useState([{ name: "Event 1" }, { name: "Event 2" }, { name: "Event 3" }]);
+
+
+  useEffect(() => {
+    // TODO: disable this if there's not selected scenario
+    if (!simulationInput.selectedScenario) {
+      return;
+    }
+
+    Axios.defaults.baseURL = import.meta.env.VITE_SERVER_ADDRESS;
+    Axios.defaults.withCredentials = true;
+
+    Axios.get("/runSimulation/exploration", {
+      params: {
+        scenarioId: simulationInput.selectedScenario,
+      }
+    }).then((response) => {
+      const data = response.data;
+      const isRothEnabled = data.isRothEnabled;
+      const allEvents = data.events.map((event) => {
+        return {
+          name: event.name,
+          id: event.id,
+        }
+      });
+
+      const allIncomeExpenseEvents = data.incomeExpenseEvents.map((event) => {
+        return {
+          name: event.name,
+          id: event.id,
+        }
+      });
+
+      const allInvestEvents = data.investEvents.map((event) => {
+        return {
+          name: event.name,
+          id: event.id,
+        }
+      });
+
+      setparameterOptions(() => {
+        const options = []
+        if (allEvents.length > 0) {
+          options.push({ value: "Start Year", label: "Start Year" }, { value: "Duration", label: "Duration" },)
+        }
+        if (allIncomeExpenseEvents.length > 0) {
+          options.push({ value: "Initial Amount", label: "Initial Amount" })
+        }
+        if (allInvestEvents.length > 0) {
+          options.push({ value: "Investment Percentage", label: "First of Two Investments" })
+        }
+        if (isRothEnabled) {
+          options.push({ value: "Roth Conversion", label: "Roth Conversion" });
+        }
+        return options;
+      })
+      setEvents(allEvents);
+      setincomeExpenseEvents(allIncomeExpenseEvents);
+      setInvestEvents(allInvestEvents);
+      setSimulationInput(() => ({
+        numSimulations: simulationInput.numSimulations,
+        selectedScenario: simulationInput.selectedScenario
+      }));
+      setSelectRemount((prevKey) => prevKey + 1);
+    }).catch((error) => {
+      if (error.response?.status === 403 || error.response?.status === 401) {
+        alert("You do not have permission to view this scenario.");
+      } else {
+        alert("Error fetching scenario name. Please try again.");
+      }
+      console.error('Error fetching scenario name:', error);
+    });
+  }, [simulationInput.selectedScenario, simulationInput.numSimulations, setSelectRemount, setSimulationInput]);
+
+  useEffect(() => {
+    const parameterValue = simulationInput[`parameter${parameterIndex}`];
+    if (parameterValue === "Start Year") {
+      setdisplayedEvents(events);
+    } else if (parameterValue === "Duration") {
+      setdisplayedEvents(events);
+    } else if (parameterValue === "Initial Amount") {
+      setdisplayedEvents(incomeExpenseEvents);
+    } else if (parameterValue === "Investment Percentage") {
+      setdisplayedEvents(investEvents);
+    } else {
+      setdisplayedEvents([]);
+    }
+  }, [parameterIndex, simulationInput, events, incomeExpenseEvents, investEvents]);
 
   return (
     <div>
@@ -31,8 +122,8 @@ const ChartParameters = ({ parameterIndex, selectRemount, simulationInput, handl
             Select Event Series {parameterIndex}
             <Select
               key={selectRemount}
-              options={eventSeries.map((event) => ({ value: event.name, label: event.name }))}
-              onChange={(option) => handleSelectChange(option, `eventSeries${parameterIndex}`)}
+              options={displayedEvents.map((event) => ({ value: event.id, label: event.name }))}
+              onChange={(option) => handleSelectChange(option, `displayedEvents${parameterIndex}`)}
               className="select"
             />
           </label>
@@ -75,7 +166,9 @@ ChartParameters.propTypes = {
   selectRemount: PropTypes.number.isRequired,
   simulationInput: PropTypes.object.isRequired,
   handleChange: PropTypes.func.isRequired,
-  handleSelectChange: PropTypes.func.isRequired
+  handleSelectChange: PropTypes.func.isRequired,
+  setSelectRemount: PropTypes.func.isRequired,
+  setSimulationInput: PropTypes.func.isRequired,
 };
 
 export default ChartParameters;
