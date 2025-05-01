e93a51ce91b1c14bf859db475088d3de3681af0e
Adrian Heath <adrian.heath@stonybrook.edu>
2025-05-01 18:43:55 -0400

    Roth limited by post-tax contribution limit

--- server/computation/simulationHelper/investmentHelper.js
+++ server/computation/simulationHelper/investmentHelper.js
@@ -182,7 +182,7 @@ export async function updateInvestments(investmentTypes) {
 
     return curYearIncome;
 }
-export async function performRothConversion(curYearIncome, curYearSS, federalIncomeTax, currentYear, birthYear, orderedRothStrategy, investmentTypes) {
+export async function performRothConversion(curYearIncome, curYearSS, federalIncomeTax, currentYear, birthYear, orderedRothStrategy, investmentTypes, annualPostTaxContributionLimit) {
 
     const age = currentYear - birthYear;
     const curYearFedTaxableIncome = curYearIncome - 0.15 * curYearSS;
@@ -198,8 +198,7 @@ export async function performRothConversion(curYearIncome, curYearSS, federalInc
     let rc = u - curYearFedTaxableIncome;
     if (rc <= 0) return { curYearIncome, curYearEarlyWithdrawals: 0 };
 
-    let remainingRC = rc;
-
+    let remainingRC = Math.min(rc, annualPostTaxContributionLimit);
     // Batch fetch investments and build a map for quick access
     const allInvestmentIds = orderedRothStrategy.map(id => typeof id === 'object' ? id._id : id).filter(id => id !== undefined);
     const investments = await investmentFactory.readMany(allInvestmentIds);
