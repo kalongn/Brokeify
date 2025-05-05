6a1d92863f86e5d06ee7685632a0fd78f1081ce8
sangeetavarri <sangeetavarri@gmail.com>
2025-05-05 15:43:25 -0400

    Added orientation

--- client/src/components/Tooltip.jsx
+++ client/src/components/Tooltip.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import styles from './tooltip.module.css';
 import { IoInformationCircleOutline } from "react-icons/io5";
 
-/*NOte: I used ChatGPT to generate overall. Modified it slightly - speciically the icon used*/
-/*prompt: Please design a react component  for a tooltip icon. White text over black squarish text bubble that displays 
-over hover.  **/
-const Tooltip = ({ text }) => {
+/* NOte: I used ChatGPT to generate overall. Modified it slightly - specifically the icon used */
+/* prompt: Please design a react component for a tooltip icon. White text over black squarish text bubble that displays over hover. **/
+
+const Tooltip = ({ text, orientation = 'above' }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
@@ -14,12 +14,12 @@ const Tooltip = ({ text }) => {
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
     >
-      {/* Info icon (using Unicode "i" character as an icon) */}
-      <i className={styles.icon}><  IoInformationCircleOutline /></i>
+      {/* Info icon */}
+      <i className={styles.icon}><IoInformationCircleOutline /></i>
 
       {/* Tooltip text */}
       <div
-        className={styles.tooltipText}
+        className={`${styles.tooltipText} ${styles[orientation]}`} // Add conditional class for orientation
         style={{
           visibility: isVisible ? 'visible' : 'hidden',
         }}
