194c5c6fe34a640be616f02c857097b0ead06e97
04mHuang <71652653+04mHuang@users.noreply.github.com>
2025-05-05 20:37:00 -0400

    [Frontend] Form modals (#146)
    * Modified: to accomodate state modal in basic info
    
    * Added: upload icon
    
    * Modified: Removed upload in modal; support redirect in new component
    
    * Modified: Modal visible only without errors and added upload to backend on continue
    
    * Updated: middleware for state tax validation
    
    * Modified: removed console.log
    
    * Modified: modal text
    
    ---------
    
    Co-authored-by: Ka Long Ngai <kls8162@gmail.com>

--- client/src/components/ModalImport.jsx
+++ client/src/components/ModalImport.jsx
@@ -2,17 +2,25 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Axios from "axios";
+import { FaUpload } from "react-icons/fa";
 
 import ModalBase from "./ModalBase";
 import styles from "./ModalImport.module.css";
 import buttonStyles from "../pages/ScenarioForm.module.css";
 
 const ModalImport = ({ isOpen, onClose }) => {
-  // Modal reused between scenario import and profile state tax upload
-  const location = useLocation();
+  const path = useLocation().pathname;
   const navigate = useNavigate();
-  const title = location.pathname === "/Home" ? "Import Scenario" : "Upload YAML file";
-  const description = location.pathname === "/Home" ? "Import a scenario from a YAML file." : "Upload state taxes from a YAML file.";
+
+  let title = "";
+  let description = "";
+  if (path === "/Home") {
+    title = "Import Scenario";
+    description = "Import a scenario from a YAML file.";
+  } else if (path === "/Profile") {
+    title = "Upload State Taxes";
+    description = "Upload state taxes from a YAML file.";
+  } 
 
   const [file, setFile] = useState(null);
   const [status, setStatus] = useState(null);
@@ -31,22 +39,22 @@ const ModalImport = ({ isOpen, onClose }) => {
         const formData = new FormData();
         formData.append("file", file);
         const response = await Axios.post(
-          location.pathname === "/Home" ? `/scenario/import` : `/stateTax/import`,
+          path === "/Home" ? `/scenario/import` : `/stateTax/import`,
           formData,
           { headers: { "Content-Type": "multipart/form-data" } }
         );
         console.log(response.data);
-        if (location.pathname === "/Home") {
+        if (path === "/Home") {
           // Redirect to the scenario simulation page
           const scenarioId = response.data.scenarioId;
           navigate(`/Scenario/${scenarioId}`);
-        } else if (location.pathname === "/Profile") {
+        } else if (path === "/Profile") {
           handleClose();
         } else {
           throw new Error("Unknown path");
         }
       } catch (error) {
-        if (location.pathname === "/Profile" && error.response?.status === 409) {
+        if (path === "/Profile" && error.response?.status === 409) {
           setStatus("A tax of the same state, same year, same filing status already exists.");
         } else if (location.pathname === "/Profile" && error.response?.status === 400) {
           setStatus("Invalid tax data.");
@@ -55,6 +63,8 @@ const ModalImport = ({ isOpen, onClose }) => {
         }
         console.error("Error uploading file:", error);
       }
+    } else {
+      setStatus("No file selected");
     }
   };
   const handleClose = () => {
@@ -65,13 +75,12 @@ const ModalImport = ({ isOpen, onClose }) => {
 
   return (
     <ModalBase isOpen={isOpen} onClose={handleClose} id={styles.modal}>
-      <h2 className={styles.title}>{title}</h2>
-      <p>{description}</p>
+      <h2>{title}</h2>
+      <p style={{ whiteSpace: "pre-line" }}>{description}</p>
       <input type="file" accept=".yaml, .yml" onChange={handleFileChange} />
       {status && <p>{status}</p>}
       <div id={buttonStyles.navButtons}>
-        <button onClick={handleClose} className={buttonStyles.deemphasizedButton}>Cancel</button>
-        <button onClick={handleUpload} className={buttonStyles.emphasizedButton}>Upload File</button>
+        <button onClick={handleUpload} className={`${buttonStyles.emphasizedButton} ${styles.uploadButton}`}><FaUpload /> Upload File</button>
       </div>
     </ModalBase>
   );
