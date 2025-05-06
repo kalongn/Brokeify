a40f40a8a465f1c52cb11eba3514e602738c8570
Michelle <michelleh11219@gmail.com>
2025-05-05 21:13:10 -0400

    Modified: Removed navigation function

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,33 +1,21 @@
 import PropTypes from "prop-types";
-import { useLocation, useNavigate } from "react-router-dom";
 
 import styles from "./Progress.module.css";
 const ProgressBar = ({ currentSectionIndex, sections }) => {
   const currProgress = (currentSectionIndex + 1) / sections.length;
-  const path = useLocation().pathname;
-  const pathSegments = path.split("/").filter(Boolean);
-  // const pathSection = pathSegments.slice(-1);
-  const navigate = useNavigate();
-
-  const handleClick = (index) => {
-    pathSegments.pop();
-    pathSegments.push(sections[index].path);
-    navigate(`/${pathSegments.join("/")}`);
-  }
 
   return (
     <div className={styles.barContainer}>
       <div className={styles.bar}>
         {sections.map((section, index) => (
-          <button
+          <div
             key={index}
             style={{ width: `${currProgress * 100}%` }}
             className={`${styles.barUnit} ${index <= currentSectionIndex ? styles.filled : ''}`}
-            onClick={() => handleClick(index)}
           >
             <div className={`${styles.node} ${index <= currentSectionIndex ? styles.filled : ''}`}></div>
             <p className={styles.nodeText}>{section.label}</p>
-          </button>
+          </div>
         ))}
       </div>
     </div>
